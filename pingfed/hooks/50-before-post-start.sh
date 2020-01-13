5afd186045a3fd7ec3ee2e7e0117a3be9c6e94ce
Chris Price <cprice@pingidentity.com>
2020-01-13 15:53:02 -0800

    Update 50-before-post-start.sh

--- pingfed/hooks/50-before-post-start.sh
+++ pingfed/hooks/50-before-post-start.sh
@@ -13,3 +13,5 @@
     sed -e "s#<construct class=\"org.sourceid.oauth20.domain.ClientManagerXmlFileImpl\"/>#<construct class=\"org.sourceid.oauth20.domain.ClientManagerLdapImpl\"/>#" \
         -e "s#<create-instance class=\"org.sourceid.oauth20.token.AccessGrantManagerJdbcImpl\"/>#<create-instance class=\"org.sourceid.oauth20.token.AccessGrantManagerLDAPPingDirectoryImpl\"/>#" \
         "/opt/out/instance/server/default/conf/META-INF/hivemodule.xml" > "/opt/out/instance/server/default/conf/META-INF/hivemodule.xml-modified"
+
+mv /opt/out/instance/server/default/conf/META-INF/hivemodule.xml-modified /opt/out/instance/server/default/conf/META-INF/hivemodule.xml
